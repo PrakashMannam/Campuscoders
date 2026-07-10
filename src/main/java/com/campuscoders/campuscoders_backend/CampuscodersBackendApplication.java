166da97368b715175f753a43d496aa763492b581
PrakashMannam <prakashmannam8@gmail.com>
2026-07-10 13:38:09 +0530

    Initial Commit

---
+++ src/main/java/com/campuscoders/campuscoders_backend/CampuscodersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campuscoders.campuscoders_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuscodersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuscodersBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campuscoders/campuscoders_backend/CampuscodersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuscoders.campuscoders_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuscodersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
