eb07fb8dd32a82785413efd07b846d4c8181779d
PrakashMannam <prakashmannam8@gmail.com>
2026-07-21 23:19:10 +0530

    feat: add jwt authentication foundation

--- backend/src/main/java/com/campuscoders/backend/auth/AuthService.java
+++ backend/src/main/java/com/campuscoders/backend/auth/AuthService.java
@@ -7,21 +7,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.campuscoders.backend.security.JwtService;
 import com.campuscoders.backend.user.Role;
 import com.campuscoders.backend.user.User;
 import com.campuscoders.backend.user.repository.UserRepository;
+
 
 @Service
 public class AuthService {
   
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
 
-  public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+  public AuthService(
+      UserRepository userRepository,
+      PasswordEncoder passwordEncoder,
+      JwtService jwtService) {
+
     this.userRepository = userRepository;
     this.passwordEncoder = passwordEncoder;
+    this.jwtService = jwtService;
   }
   
+  //Response for registration
   public AuthResponse register(RegisterRequest registerRequest) {
     if (userRepository.existsByEmail(registerRequest.getEmail())) {
       throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already exists");
@@ -40,10 +49,35 @@ public class AuthService {
 
     AuthResponse response = new AuthResponse();
 
-    response.setToken(null);
+    response.setToken(jwtService.generateToken(savedUser));
     response.setEmail(savedUser.getEmail());
     response.setFullName(savedUser.getFullName());
     response.setRole(savedUser.getRole());
+
+    return response;
+  }
+
+  //Response for login
+  public AuthResponse login(LoginRequest request) {
+    User user = userRepository.findByEmail(request.getEmail())
+        .orElseThrow(() ->
+            new ResponseStatusException(
+                HttpStatus.UNAUTHORIZED,
+                "Invalid email or password"
+            )
+        );
+    if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+      throw new ResponseStatusException(
+          HttpStatus.UNAUTHORIZED,
+          "Invalid email or password");
+    }
+    
+    AuthResponse response = new AuthResponse();
+
+    response.setToken(jwtService.generateToken(user));
+    response.setEmail(user.getEmail());
+    response.setFullName(user.getFullName());
+    response.setRole(user.getRole());
 
     return response;
   }
